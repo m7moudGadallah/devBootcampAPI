876f07a2fc1cff739dbf9e163ed7f7afd9945987
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-07-17 18:06:03 +0300

    send responses to server with different types of data

--- server.js
+++ server.js
@@ -5,6 +5,29 @@ const PORT = 5000;
 const server = http.createServer((req, res) => {
     const { headers, url, method } = req;
     console.log({ headers, url, method });
+
+    // res.setHeader('X-Powered-By', 'Node.js'); // when we use not standard header we add 'X-' prefix before header name
+
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.write('Hello :)');
+
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('<h1> Hello :) </h1>');
+
+    // res.setHeader('Content-Type', 'application/json');
+
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Node.js',
+    });
+    
+    res.end(
+        JSON.stringify({
+            success: true,
+            data: 'hello :)',
+        })
+    );
+
     res.end();
 });
 
