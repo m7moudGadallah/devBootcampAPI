e0d0e31417a1b54fe0051c6b26c569c5d19620f5
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-07-19 07:02:15 +0300

    Enhance sendSuccessResonse utils
    - Enhance JSON utils by adding results param

--- utils/sendSuccessResponse.js
+++ utils/sendSuccessResponse.js
@@ -6,20 +6,23 @@
  * @param {number} [options.statusCode=200] - The status code for the response.
  * @param {object} [options.data] - The data to be included in the response.
  * @param {string} [options.message] - The message to be included in the response.
+ * @param {number} [options.results] - The number of data items in the response.
  * @example
     const options = {
     response: res,
     statusCode: 200,
     data: { name: 'John', age: 30 },
     message: 'Success'
+    results: 1
     };
     JSON(options);
 */
-const JSON = ({ response, statusCode = 200, data, message }) => {
+const JSON = ({ response, statusCode = 200, data, message, results }) => {
     response.status(statusCode).json({
         success: true,
-        data,
         message,
+        results,
+        data,
     });
 };
 
