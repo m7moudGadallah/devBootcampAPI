902576f70635f79542e4ba39023ad44e31773ed3
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-07-21 10:57:49 +0300

    Enhance seeder script
    - Enhance 2 options of load model data & delete model data
    - Add 2 new options loadAll and delete all

--- dev-data/seeder.js
+++ dev-data/seeder.js
@@ -13,52 +13,92 @@ const { DATABASE, DATABASE_PASSWORD } = process.env;
 // connect to database
 db.connectDB({ DATABASE, DATABASE_PASSWORD });
 
-// load data
-const modelName = process.argv[2][0].toUpperCase() + process.argv[2].slice(1).toLowerCase();
-const model = models[modelName];
-const fileName = `${modelName.toLowerCase()}s`;
-const data = require(`./backup/${fileName}.json`);
-
 // import data on database
-const importData = async function () {
+const importData = async function (modelName) {
+    const model = models[modelName];
+    const fileName = `${modelName.toLowerCase()}s`;
+    const data = require(`./backup/${fileName}.json`);
+
     try {
         await model.create(data, { validateBeforeSave: false });
         console.log(
-            'Data loaded successfully🎉...'.green.underline.bold.italic
+            `${modelName}s loaded successfully🎉...`.green.underline.bold.italic
         );
     } catch (err) {
         console.log(`Error💥, ${err.message}`.red.underline.bold.italic);
-    } finally {
-        process.exit();
     }
 };
 
 //delete data
-const deleteData = async function () {
+const deleteData = async function (modelName) {
     try {
+        const model = models[modelName];
         await model.deleteMany();
         console.log(
-            'Data deleted successfully🎉...'.green.underline.bold.italic
+            `${modelName}s deleted successfully🎉...`.green.underline.bold
+                .italic
         );
     } catch (err) {
         console.log(`Error💥, ${err.message}`.red.underline.bold.italic);
-    } finally {
-        process.exit();
+    }
+};
+
+const loadAll = async function () {
+    try {
+        for (const modelName of Object.keys(models)) {
+            await importData(modelName);
+        }
+    } catch (err) {
+        console.log(`Error💥, ${err.message}`.red.underline.bold.italic);
+    }
+};
+
+const deleteAll = async function () {
+    try {
+        for (const modelName of Object.keys(models)) {
+            await deleteData(modelName);
+        }
+    } catch (err) {
+        console.log(`Error💥, ${err.message}`.red.underline.bold.italic);
     }
 };
 
 // check passed option
-if (process.argv[3] === '--import' || process.argv[3] === '-i') {
-    importData();
-} else if (process.argv[3] === '--delete' || process.argv[3] === '-d') {
-    deleteData();
-} else {
-    console.log(`Error💥, undefined command`.red.underline.bold.italic);
-}
+(async () => {
+    if (process.argv.length <= 4) {
+        if (process.argv[2] === '--import' || process.argv[2] === 'i') {
+            if (process.argv.length >= 4) {
+                const modelName =
+                    process.argv[3][0].toUpperCase() +
+                    process.argv[3].slice(1).toLowerCase();
+                await importData(modelName);
+            } else {
+                await loadAll();
+            }
+        } else if (process.argv[2] === '--delete' || process.argv[2] === 'd') {
+            if (process.argv.length >= 4) {
+                const modelName =
+                    process.argv[3][0].toUpperCase() +
+                    process.argv[3].slice(1).toLowerCase();
+                await deleteData(modelName);
+            } else {
+                await deleteAll();
+            }
+        }
+    } else {
+        console.log(`Error💥, undefined command`.red.underline.bold.italic);
+    }
+    process.exit();
+})();
 
 /*
     How to execute script?
     
-    1) importing: node seeder.js {model} (--import  OR -i)
-    2) clear: node seeder.js {model} (--delete OR -d)
+    run: node seeder.js
+
+    options:
+    1) import all: [i]
+    2) delete all: [d]
+    3) import model: [i {modelName}]
+    4) delete model: [d {modelName}]
 */
