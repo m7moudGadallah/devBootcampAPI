71212ed7752fba9ab508cd7a0075b1583ea5498d
m7moudGadallah <mahmoudgadallah876@gmail.com>
2023-07-24 13:34:06 +0300

    Refactor courses [routes & controller]

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -2,6 +2,12 @@ const { Course } = require('../models');
 const CRUDFactory = require('./CRUDFactory');
 const factory = new CRUDFactory(Course, { docName: 'course' });
 
+/**
+ * @route GET /api/v1/courses
+ * @route GET /api/v1/bootcamps/:bootcampId/courses
+ * @desc Middleware that used to Set bootcampId in request query before calling controller
+ * @access public
+ */
 const setBootcampId = (req, res, next) => {
     if (req.params?.bootcampId) {
         req.query.bootcamp = req.params.bootcampId;
@@ -12,12 +18,17 @@ const setBootcampId = (req, res, next) => {
 };
 
 /**
- * Get all courses from the database and send a success response with the courses data.
+ * @route GET /api/v1/courses
+ * @route GET /api/v1/bootcamps/:bootcampId/courses
+ * @desc Get all courses from the database and send a success response with the courses data.
+ * @access public
  */
 const getAllCourses = factory.getAll({ sortByFields: '-createdAt' });
 
 /**
- * Get a single course by ID from the database and send a success response with the course data.
+ * @route GET /api/v1/courses/:id
+ * @desc Get a single course by ID from the database and send a success response with the course data.
+ * @access public
  */
 const getCourse = factory.getOne({
     populates: [
@@ -29,17 +40,27 @@ const getCourse = factory.getOne({
 });
 
 /**
- * Create a new course and send a success response with the created course data.
+ * @route POST /api/v1/courses
+ * @route POST /api/v1/bootcamps/:bootcampId/courses
+ * @desc create a Course
+ * @access private
+ * @auth ['admin']
  */
 const createCourse = factory.createOne();
 
 /**
- * Update a course by ID in the database and send a success response with the updated course data.
+ * @route PATCH /api/v1/courses/:id
+ * @desc Update a course by ID in the database and send a success response with the updated course data.
+ * @access private
+ * @auth ['admin']
  */
 const updateCourse = factory.updateOne();
 
 /**
- * Delete a course by ID from the database and send a success response with a null data.
+ * @route DELETE /api/v1/courses/:id
+ * @desc Delete a course by ID from the database and send a success response with a null data.
+ * @access private
+ * @auth ['admin']
  */
 const deleteCourse = factory.deleteOne();
 
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,48 +1,15 @@
 const router = require('express').Router({ mergeParams: true });
 const { courseController } = require('../controllers');
 
-/**
- * @route GET /api/v1/courses
- * @route GET /api/v1/bootcamps/:bootcampId/courses
- * @desc get all courses
- * @access public
- */
 router
     .route('/')
-    .get(courseController.setBootcampId, courseController.getAllCourses);
-
-/**
- * @route GET /api/v1/courses/:id
- * @desc get a bootcamp by id
- * @access public
- */
-router.route('/:id').get(courseController.getCourse);
-
-/**
- * @route POST /api/v1/courses
- * @route POST /api/v1/bootcamps/:bootcampId/courses
- * @desc create a Course
- * @access private
- * @auth ['admin']
- */
-router
-    .route('/')
+    .get(courseController.setBootcampId, courseController.getAllCourses)
     .post(courseController.setBootcampId, courseController.createCourse);
 
-/**
- * @route PATCH /api/v1/courses/:id
- * @desc update a Course
- * @access private
- * @auth ['admin']
- */
-router.route('/:id').patch(courseController.updateCourse);
-
-/**
- * @route DELETE /api/v1/courses/:id
- * @desc delete a Course
- * @access private
- * @auth ['admin']
- */
-router.route('/:id').delete(courseController.deleteCourse);
+router
+    .route('/:id')
+    .get(courseController.getCourse)
+    .patch(courseController.updateCourse)
+    .delete(courseController.deleteCourse);
 
 module.exports = router;
